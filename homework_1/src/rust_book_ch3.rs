6a18984db856e9c4fdfeadf09381dedd958572b9
Rustam Shamsutdinov <sypoo@pop-os.localdomain>
2023-08-28 22:09:46 +0300

    test ci

--- homework_1/src/rust_book_ch3.rs
+++ homework_1/src/rust_book_ch3.rs
@@ -344,11 +344,11 @@
 
 
 
-// fn main() {
-//     let a = [5; 10];
-//     let mut sum = 0;
-//     for x in a {
-//         sum += x;
-//     }
-//     println!("{sum}");
-// }
+fn main() {
+    let a = [5; 10];
+    let mut sum = 0;
+    for x in a {
+        sum += x;
+    }
+    println!("{sum}");
+}
